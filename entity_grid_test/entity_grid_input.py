aa713cd21a5c42deab60e96fcb875e3b1a9a23a3
Mei <chenmeili070308@gmail.com>
2021-10-30 01:06:14 -0400

    add entity grid test

---
+++ entity_grid_test/entity_grid_input.py
@@ -0,0 +1,40 @@
+import collections
+import glob
+import json
+import sys
+import os
+
+SUBSETS = "train dev test".split()
+
+def get_text_block(sentences):
+  return " ".join([x["text"] + '\n' for x in sentences])
+
+def get_datasets():
+  dataset_dir = "../peer-review-discourse-dataset/data_prep/final_dataset/"
+
+  datasets = collections.defaultdict(list)
+
+  for subset in SUBSETS:
+    for filename in glob.glob(dataset_dir + subset + "/*"):
+      with open(filename, 'r') as f:
+        datasets[subset].append(json.load(f))
+
+  return datasets
+
+
+def main():
+  datasets = get_datasets()
+
+  if not os.path.exists('text/'):
+    os.makedirs('text/')
+
+  text_dir = 'text/'
+  for pair in datasets['train'][:10]:
+    filename = pair['metadata']['review_id'] + '.txt'
+    text_block = get_text_block(pair['review_sentences'])
+    out_file = open(text_dir + filename, 'w')
+    out_file.write(text_block)
+    out_file.close()
+
+if __name__ == "__main__":
+  main()
---
+++ entity_grid_test/entity_grid_output.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import glob
+import os
+
+def main():
+  if not os.path.exists('output/'):
+    os.makedirs('output/')
+
+  text_dir = 'text/'
+  grid_dir = 'grid/'
+  output_dir = 'output/'
+  for filename in glob.glob(text_dir + "/*"):
+    output_csv = pd.DataFrame()
+    with open(filename, 'r') as f:
+      output_csv['rr_sentences'] = f.readlines()
+
+    grid_filename = grid_dir + filename[-14:]
+    with open(grid_filename, 'r') as f:
+      for line in f.readlines():
+        line_list = line.split(' ')
+        word = line_list[0]
+        output_csv[word] = line_list[1:-1]
+    output_csv.to_csv(output_dir + filename[-14:-4] + '.csv')
+
+
+if __name__ == "__main__":
+  main()
